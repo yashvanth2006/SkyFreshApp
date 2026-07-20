168b93f5a9d36bed561744b3707df28bfdbc8545
yashvanth2006 <yashvanth2006k@gmail.com>
2026-07-20 22:43:40 +0530

    localhost3000

--- skyfresh-backend/server.js
+++ skyfresh-backend/server.js
@@ -8,7 +8,7 @@ const cors = require('cors');
 const app = express();
 
 app.use(cors({
-  origin: ['http://localhost:3000', 'http://localhost:3001'],
+  origin: ['http://localhost:3000', 'http://127.0.0.1:3000'],
   methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
