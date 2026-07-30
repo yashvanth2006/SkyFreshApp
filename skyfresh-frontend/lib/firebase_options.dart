4ad5dc5f58f3754b419aaedee633db19d7717331
yashvanth2006 <yashvanth2006k@gmail.com>
2026-07-31 00:38:56 +0530

    otp

---
+++ skyfresh-frontend/lib/firebase_options.dart
@@ -0,0 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
+
+// Default FirebaseOptions for current platform
+class DefaultFirebaseOptions {
+  static FirebaseOptions get currentPlatform {
+    return const FirebaseOptions(
+      apiKey: 'AIzaSyDhjuA8mWA5wv2azz4wikisliezetECmus',
+      appId: '1:392048098425:android:5b71912a0d8f886809a8a8',
+      messagingSenderId: '392048098425',
+      projectId: 'skyfresh-3ddad',
+    );
+  }
+}
