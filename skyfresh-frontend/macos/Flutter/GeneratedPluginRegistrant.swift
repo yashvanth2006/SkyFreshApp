797b5258777b04d1c13a542d13743ae2b5c587f3
yashvanth2006 <yashvanth2006k@gmail.com>
2026-07-31 01:11:36 +0530

    otp

--- skyfresh-frontend/macos/Flutter/GeneratedPluginRegistrant.swift
+++ skyfresh-frontend/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -11,6 +11,7 @@ import firebase_messaging
 import flutter_secure_storage_darwin
 import geocoding_darwin
 import geolocator_apple
+import google_sign_in_ios
 import package_info_plus
 import shared_preferences_foundation
 import sqflite_darwin
@@ -23,6 +24,7 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FlutterSecureStorageDarwinPlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStorageDarwinPlugin"))
   GeocodingDarwinPlugin.register(with: registry.registrar(forPlugin: "GeocodingDarwinPlugin"))
   GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
+  FLTGoogleSignInPlugin.register(with: registry.registrar(forPlugin: "FLTGoogleSignInPlugin"))
   FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
